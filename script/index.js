8d2fee53681bf2539cf135204a82ed3fda819d64
Saniul <isaniul999@gmail.com>
2025-09-05 12:50:29 +0600

    added modal

--- script/index.js
+++ script/index.js
@@ -25,24 +25,50 @@ const displayLevelData=(data)=>{
     }
     data.forEach(word=>{
         const card=document.createElement('div');
-        // "id": 43,
-        // "level": 5,
-        // "word": "Radiant",
-        // "meaning": "উজ্জ্বল / দীপ্তিময়",
-        // "pronunciation": "রেডিয়ান্ট"
         card.innerHTML=`
          <div class="bg-white shadow-sm py-10 px-5 rounded-xl text-center space-y-2 ">
             <h2 class="font-bold text-3xl">${word.word?word.word:"NOT FOUND"}</h2>
             <p class="font-medium text-xl">Meaning /Pronounciation</p>
             <div class="text-2xl font-medium font-bangla">"${word.meaning? word.meaning :"NOT FOUND" } / ${word.pronunciation? word.pronunciation: "Pronuntiation NOT FOUND"}"</div>
             <div class="flex justify-between items-center">
-                <button class="btn bg-[rgb(26_145_255/0.1)] hover:bg-[rgb(26_145_255/80)]"><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="boxModal(${word.id})" class="btn bg-[rgb(26_145_255/0.1)] hover:bg-[rgb(26_145_255/80)]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[rgb(26_145_255/0.1)] hover:bg-[rgb(26_145_255/80)]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
       </div>
         `;
         wordContainer.appendChild(card);
     });
+}
+const boxModal=(id)=>{
+    const modelBox=document.getElementById("modal-container");
+    let boxInModal=document.createElement('div');
+    const url=`https://openapi.programming-hero.com/api/word/${id}`;
+    fetch(url).then(res=>res.json()).then(combine=>{
+        // console.log(combine.data.word);
+        boxInModal.innerHTML=`
+             <h2 class="text-2xl font-bold">${combine.data.word}(<i class="fa-solid fa-microphone-lines"></i>:${combine.data.pronunciation})</h2>
+          <div>
+            <h3 class="text-lg font-bold">Meaning</h3>
+            <p class="py-4">${combine.data.meaning}</p>
+          </div>
+           <div>
+            <h3 class="text-lg font-bold">Example</h3>
+            <p class="py-4">${combine.data.sentence}</p>
+          </div>
+           <div>
+            <h3 class="text-lg font-bold">সমার্থক শব্দ গুলো</h3>
+            <p class="py-4">many</p>
+          </div>
+          <div class="modal-action">
+            <form method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
+            </form>
+          </div>
+        `;
+    });
+    modelBox.appendChild(boxInModal);
+    my_modal.showModal();
 }
 const readData=(leason)=>{
     const btnParent=document.getElementById('btn-container');
